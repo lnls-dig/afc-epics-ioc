8759074c7b5367a2bc577a644284e1610e317c32
Érico Nogueira <erico.rolim@lnls.br>
2023-04-05 14:53:54 -0300

    iocutca: change TRIGGER_NAME for LAMP triggers.
    Instead of using a generic TRIGGER prefix for this trigger core, specify
    that it is connected to the LAMP acquisition module.

--- iocBoot/iocutca/triggers.cmd
+++ iocBoot/iocutca/triggers.cmd
@@ -2,7 +2,7 @@ TriggerIface(0)
 dbLoadTemplate("db/trigger_iface.substitutions", "P=${P}, R=${R}, TRIGGER_NAME=TRIGGER, PORT=TRIGGER_IFACE-0")
 
 TriggerMux(0)
-dbLoadTemplate("db/trigger_mux.substitutions", "P=${P}, R=${R}, TRIGGER_NAME=TRIGGER, PORT=TRIGGER_MUX-0")
+dbLoadTemplate("db/trigger_mux.substitutions", "P=${P}, R=${R}, TRIGGER_NAME=TRIGGER_LAMP, PORT=TRIGGER_MUX-0")
 TriggerMux(1)
 dbLoadTemplate("db/trigger_mux.substitutions", "P=${P}, R=${R}, TRIGGER_NAME=TRIGGER_DCCFMC, PORT=TRIGGER_MUX-1")
 TriggerMux(2)
