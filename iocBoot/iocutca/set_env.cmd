986bf0c90cd19d4ea9646c9991dd6a38aa6e479e
Érico Nogueira <erico.rolim@lnls.br>
2023-04-05 14:58:02 -0300

    iocutca: don't hardcode device information.
    Read them from environment variables, which will be provided by a future
    runFOFB.sh script.

--- iocBoot/iocutca/set_env.cmd
+++ iocBoot/iocutca/set_env.cmd
@@ -1,5 +1,5 @@
-epicsEnvSet("P", "DE-23SL11:")
-epicsEnvSet("R", "BS-FOFBCtrl:")
-epicsEnvSet("S", "SI-23")
+epicsEnvSet("P", "${AREA_PREFIX}")
+epicsEnvSet("R", "${DEVICE_PREFIX}")
+epicsEnvSet("S", "${RTM_PREFIX}")
 
 epicsEnvSet("NELM", "1000000")
