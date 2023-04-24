114e3e950aa9b69d6951f7d168018a96c06f58a1
Érico Nogueira <erico.rolim@lnls.br>
2023-04-24 17:41:19 -0300

    rtmlamp: add ACQ_NAME to rtmlamp_data waveforms.

--- iocBoot/iocutca/rtmlamp.cmd
+++ iocBoot/iocutca/rtmlamp.cmd
@@ -1,2 +1,2 @@
 RtmLamp(0)
-dbLoadTemplate("db/rtmlamp.substitutions", "P=${P}, R=${R}, S=${S}, NELM=${NELM}, PORT=RTMLAMP-0, ACQ_PORT=ACQ-0")
+dbLoadTemplate("db/rtmlamp.substitutions", "P=${P}, R=${R}, S=${S}, NELM=${NELM}, ACQ_NAME=LAMP, PORT=RTMLAMP-0, ACQ_PORT=ACQ-0")
