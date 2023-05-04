74ea76c8487e09bb46c66050f5630b6d152fe589
Érico Nogueira <erico.rolim@lnls.br>
2023-05-04 16:05:29 -0300

    iocutca: instantiate sysid module.
    The new templates could only be added to sysid.substitutions in this
    commit, since the SYSID port was instantiated here.

--- iocBoot/iocutca/sysid.cmd
+++ iocBoot/iocutca/sysid.cmd
@@ -1,1 +1,2 @@
-dbLoadTemplate("db/sysid.substitutions", "P=${P}, R=${R}, S=${S}, NELM=${NELM}, ACQ_NAME=SYSID, ACQ_PORT=ACQ-3")
+SysId(0)
+dbLoadTemplate("db/sysid.substitutions", "P=${P}, R=${R}, S=${S}, NELM=${NELM}, PORT=SYSID-0, ACQ_NAME=SYSID, ACQ_PORT=ACQ-3")
