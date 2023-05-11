lnls-dig/afc-epics-ioc

commit e59d701f1fe36ee856bdd56c9c5552ac54150d00

acq: add template files.
This includes the templates for the data vector files. We currently
support raw and rtmlamp data.

utcaApp/Db/Makefile
@@ -5,8 +5,9 @@ include $(TOP)/configure/CONFIG
 
 # Install databases, templates & substitutions like this
 DB += scan_task.template
+DB += acq.template raw_data.template
 DB += fofb_cc.template fofb_cc_channel.template fofb_cc.substitutions
-DB += rtmlamp.template rtmlamp.substitutions
+DB += rtmlamp.template rtmlamp_data.template rtmlamp.substitutions
 DB += trigger_iface.template trigger_iface.substitutions
 DB += trigger_mux.template trigger_mux.substitutions
 

utcaApp/Db/acq.template
@@ -1,0 +1,200 @@
+# ACQ records
+
+record (mbbo, "$(P)$(R)$(ACQ_NAME)DataType"){
+    field(DTYP, "asynInt32")
+    field(OUT, "@asyn($(PORT))TYPE")
+}
+
+record(longout,"$(P)$(R)$(ACQ_NAME)SamplesPre-SP"){
+    field(DTYP,"asynInt32")
+    field(DESC,"Set number of pre samples for waveforms")
+    field(DRVH,"$(NELM)")
+    field(DRVL,"0")
+    field(SCAN,"Passive")
+    field(OUT,"@asyn($(PORT))PRE_SAMPLES")
+}
+record(longin,"$(P)$(R)$(ACQ_NAME)SamplesPre-RB"){
+    field(DTYP,"asynInt32")
+    field(DESC,"Get number of pre samples for waveforms")
+    field(SCAN,"I/O Intr")
+    field(INP,"@asyn($(PORT))PRE_SAMPLES")
+}
+
+record(longout,"$(P)$(R)$(ACQ_NAME)SamplesPost-SP"){
+    field(DTYP,"asynInt32")
+    field(DESC,"Set number of post samples for waveforms")
+    field(DRVH,"$(NELM)")
+    field(DRVL,"0")
+    field(SCAN,"Passive")
+    field(OUT,"@asyn($(PORT))POST_SAMPLES")
+    field(VAL, "0")
+}
+record(longin,"$(P)$(R)$(ACQ_NAME)SamplesPost-RB"){
+    field(DTYP,"asynInt32")
+    field(DESC,"Get number of post samples for waveforms")
+    field(SCAN,"I/O Intr")
+    field(INP,"@asyn($(PORT))POST_SAMPLES")
+    field(VAL, "0")
+}
+
+record(longout,"$(P)$(R)$(ACQ_NAME)Shots-SP"){
+    field(DTYP,"asynInt32")
+    field(DESC,"Set number of shots for waveforms")
+    field(DRVH,"100000")
+    field(DRVL,"1")
+    field(SCAN,"Passive")
+    field(OUT,"@asyn($(PORT))NB")
+}
+record(longin,"$(P)$(R)$(ACQ_NAME)Shots-RB"){
+    field(DTYP,"asynInt32")
+    field(DESC,"Get number of shots for waveforms")
+    field(SCAN,"I/O Intr")
+    field(INP,"@asyn($(PORT))NB")
+}
+
+record(mbbo, "$(P)$(R)$(ACQ_NAME)Channel-Sel"){
+    field(DTYP, "asynInt32")
+    field(DESC, "Set acquisition's channel")
+    field(OUT,"@asyn($(PORT))WHICH")
+    field(SCAN,"Passive")
+}
+record(mbbi, "$(P)$(R)$(ACQ_NAME)Channel-Sts"){
+    field(DTYP, "asynInt32")
+    field(DESC, "Get acquisition's channel")
+    field(INP,"@asyn($(PORT))WHICH")
+    field(SCAN,"I/O Intr")
+}
+
+record(mbbo, "$(P)$(R)$(ACQ_NAME)Trigger-Sel"){
+    field(DTYP, "asynInt32")
+    field(DESC, "Set acquisition trigger")
+    field(SCAN,"Passive")
+    field(OUT,"@asyn($(PORT))TRIGGER")
+}
+record(mbbi, "$(P)$(R)$(ACQ_NAME)Trigger-Sts"){
+    field(DTYP, "asynInt32")
+    field(DESC, "Get acquisition trigger")
+    field(SCAN,"I/O Intr")
+    field(INP,"@asyn($(PORT))TRIGGER")
+}
+
+record(mbbo, "$(P)$(R)$(ACQ_NAME)TriggerEvent-Cmd"){
+    field(DTYP,"asynInt32")
+    field(DESC,"Trigger acquisition event")
+    field(SCAN,"Passive")
+    field(OUT,"@asyn($(PORT))EVENT")
+}
+
+record(mbbo, "$(P)$(R)$(ACQ_NAME)TriggerRep-Sel"){
+    field(DTYP, "asynInt32")
+    field(DESC, "Set repetitive acquisitions")
+    field(OUT,"@asyn($(PORT))REPETITIVE")
+    field(SCAN,"Passive")
+}
+record(mbbi, "$(P)$(R)$(ACQ_NAME)TriggerRep-Sts"){
+    field(DTYP, "asynInt32")
+    field(DESC, "Get repetitive acquisitions")
+    field(INP,"@asyn($(PORT))REPETITIVE")
+    field(SCAN,"I/O Intr")
+}
+
+record(ao, "$(P)$(R)$(ACQ_NAME)UpdateTime-SP"){
+    field(DTYP, "asynFloat64")
+    field(DESC, "Set repetitive mode update time")
+    field(EGU, "s")
+    field(DRVH,"100000")
+    field(DRVL,"0.001")
+    field(OUT,"@asyn($(PORT))UPDATE_TIME")
+    field(SCAN,"Passive")
+}
+record(ai, "$(P)$(R)$(ACQ_NAME)UpdateTime-RB"){
+    field(DTYP, "asynFloat64")
+    field(DESC, "Get repetitive mode update time")
+    field(EGU, "s")
+    field(SCAN,"I/O Intr")
+    field(INP,"@asyn($(PORT))UPDATE_TIME")
+}
+
+record(longout,"$(P)$(R)$(ACQ_NAME)TriggerDataThres-SP"){
+    field(DTYP,"asynInt32")
+    field(DESC,"set data trigger threshold")
+    field(SCAN,"Passive")
+    field(OUT,"@asyn($(PORT))TRIG_DATA_THRES")
+}
+record(longin,"$(P)$(R)$(ACQ_NAME)TriggerDataThres-RB"){
+    field(DTYP,"asynInt32")
+    field(DESC,"get data trigger threshold")
+    field(SCAN,"I/O Intr")
+    field(INP,"@asyn($(PORT))TRIG_DATA_THRES")
+}
+
+record(bo,"$(P)$(R)$(ACQ_NAME)TriggerDataPol-Sel"){
+    field(DTYP,"asynInt32")
+    field(DESC,"set data trigger polarity")
+    field(SCAN,"Passive")
+    field(OUT,"@asyn($(PORT))HW_TRIG_POL")
+}
+record(bi,"$(P)$(R)$(ACQ_NAME)TriggerDataPol-Sts"){
+    field(DTYP,"asynInt32")
+    field(DESC,"get data trigger polarity")
+    field(SCAN,"I/O Intr")
+    field(INP,"@asyn($(PORT))HW_TRIG_POL")
+}
+
+record(longout,"$(P)$(R)$(ACQ_NAME)TriggerDataSel-SP"){
+    field(DTYP,"asynInt32")
+    field(DESC,"set data trigger selection")
+    field(DRVH,"31")
+    field(DRVL,"0")
+    field(SCAN,"Passive")
+    field(OUT,"@asyn($(PORT))INT_TRIG_SEL")
+}
+record(longin,"$(P)$(R)$(ACQ_NAME)TriggerDataSel-RB"){
+    field(DTYP,"asynInt32")
+    field(DESC,"get data trigger selection")
+    field(SCAN,"I/O Intr")
+    field(INP,"@asyn($(PORT))INT_TRIG_SEL")
+}
+
+record(longout,"$(P)$(R)$(ACQ_NAME)TriggerDataHyst-SP"){
+    field(DTYP,"asynInt32")
+    field(DESC,"set data trigger hysteresis")
+    field(DRVH,"255")
+    field(DRVL,"0")
+    field(SCAN,"Passive")
+    field(OUT,"@asyn($(PORT))THRES_FILT")
+}
+record(longin,"$(P)$(R)$(ACQ_NAME)TriggerDataHyst-RB"){
+    field(DTYP,"asynInt32")
+    field(DESC,"get data trigger hysteresis")
+    field(SCAN,"I/O Intr")
+    field(INP,"@asyn($(PORT))THRES_FILT")
+}
+
+record(longout,"$(P)$(R)$(ACQ_NAME)TriggerHwDly-SP"){
+    field(DTYP,"asynInt32")
+    field(DESC,"set trigger delay")
+    field(DRVH,"2147483647")
+    field(DRVL,"0")
+    field(SCAN,"Passive")
+    field(OUT,"@asyn($(PORT))TRIG_DLY")
+}
+record(longin,"$(P)$(R)$(ACQ_NAME)TriggerHwDly-RB"){
+    field(DTYP,"asynInt32")
+    field(DESC,"get trigger delay")
+    field(SCAN,"I/O Intr")
+    field(INP,"@asyn($(PORT))TRIG_DLY")
+}
+
+record(mbbo,"$(P)$(R)$(ACQ_NAME)DataTrigChan-Sel"){
+    field(DTYP,"asynInt32")
+    field(DESC,"set data-driven trigger channel")
+    field(SCAN,"Passive")
+    field(OUT,"@asyn($(PORT))DTRIG_WHICH")
+}
+record(mbbi,"$(P)$(R)$(ACQ_NAME)DataTrigChan-Sts"){
+    field(DTYP,"asynInt32")
+    field(DESC,"get data-driven trigger channel")
+    field(SCAN,"I/O Intr")
+    field(INP,"@asyn($(PORT))DTRIG_WHICH")
+}

utcaApp/Db/raw_data.template
@@ -1,0 +1,7 @@
+record(aai, "$(P)$(R)$(ACQ_NAME)RawData"){
+    field(DTYP, "asynInt32ArrayIn")
+    field(NELM, "$(NELM)")
+    field(FTVL, "LONG")
+    field(SCAN, "I/O Intr")
+    field(INP, "@asyn($(PORT))RAW")
+}

utcaApp/Db/rtmlamp_data.template
@@ -1,0 +1,16 @@
+# RTMLAMP Data Records
+
+record(aai, "$(S)$(RTM_CHAN)CurrentRawData"){
+    field(DTYP, "asynInt16ArrayIn")
+    field(NELM, "$(NELM)")
+    field(FTVL, "SHORT")
+    field(SCAN, "I/O Intr")
+    field(INP, "@asyn($(PORT),$(ADDR))LAMP_I")
+}
+record(aai, "$(S)$(RTM_CHAN)VoltageRawData"){
+    field(DTYP, "asynInt16ArrayIn")
+    field(NELM, "$(NELM)")
+    field(FTVL, "SHORT")
+    field(SCAN, "I/O Intr")
+    field(INP, "@asyn($(PORT),$(ADDR))LAMP_V")
+}

commit ca5ac99b8528cef5edf19eed65d3f494154a520d

acq: add ScanTasks for the ACQ ports.
This was missed when the template file was first written.

Fixes: e59d701f1fe36ee856bdd56c9c5552ac54150d00

iocBoot/iocutca/acq.cmd
@@ -1,11 +1,11 @@
 Acq(0)
-dbLoadRecords("db/acq.template", "P=${P}, R=${R}, ACQ_NAME=LAMP, NELM=${NELM}, PORT=ACQ-0")
+dbLoadTemplate("db/acq.substitutions", "P=${P}, R=${R}, ACQ_NAME=LAMP, NELM=${NELM}, PORT=ACQ-0")
 Acq(1)
-dbLoadRecords("db/acq.template", "P=${P}, R=${R}, ACQ_NAME=DCCFMC, NELM=${NELM}, PORT=ACQ-1")
+dbLoadTemplate("db/acq.substitutions", "P=${P}, R=${R}, ACQ_NAME=DCCFMC, NELM=${NELM}, PORT=ACQ-1")
 dbLoadRecords("db/raw_data.template", "P=${P}, R=${R}, ACQ_NAME=DCCFMC, NELM=${NELM}, PORT=ACQ-1")
 Acq(2)
-dbLoadRecords("db/acq.template", "P=${P}, R=${R}, ACQ_NAME=DCCP2P, NELM=${NELM}, PORT=ACQ-2")
+dbLoadTemplate("db/acq.substitutions", "P=${P}, R=${R}, ACQ_NAME=DCCP2P, NELM=${NELM}, PORT=ACQ-2")
 dbLoadRecords("db/raw_data.template", "P=${P}, R=${R}, ACQ_NAME=DCCP2P, NELM=${NELM}, PORT=ACQ-2")
 Acq(3)
-dbLoadRecords("db/acq.template", "P=${P}, R=${R}, ACQ_NAME=SYSID, NELM=${NELM}, PORT=ACQ-3")
+dbLoadTemplate("db/acq.substitutions", "P=${P}, R=${R}, ACQ_NAME=SYSID, NELM=${NELM}, PORT=ACQ-3")
 dbLoadRecords("db/raw_data.template", "P=${P}, R=${R}, ACQ_NAME=SYSID, NELM=${NELM}, PORT=ACQ-3")

utcaApp/Db/Makefile
@@ -7,7 +7,7 @@ include $(TOP)/configure/CONFIG
 DB += build_info.template
 
 DB += scan_task.template
-DB += acq.template raw_data.template
+DB += acq.template raw_data.template acq.substitutions
 DB += fofb_cc.template fofb_cc_channel.template fofb_cc.substitutions
 DB += fofb_processing.template fofb_processing_channel.template fofb_processing.substitutions
 DB += rtmlamp.template rtmlamp_channel.template rtmlamp_data.template rtmlamp.substitutions

utcaApp/Db/acq.substitutions
@@ -1,0 +1,7 @@
+file db/scan_task.template {
+    {}
+}
+
+file db/acq.template {
+    {}
+}