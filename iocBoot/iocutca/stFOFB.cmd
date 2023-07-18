5373cafd7402c6ea35a7ff42db5af5a1b840fd1d
Érico Nogueira <erico.rolim@lnls.br>
2023-07-18 17:01:56 -0300

    iocutca: remove autosave_post.cmd.
    It uses file names specific to each IOC, and using variables in it would
    only further complicate things, so have each st.cmd file start autosave
    on their own.

--- iocBoot/iocutca/stFOFB.cmd
+++ iocBoot/iocutca/stFOFB.cmd
@@ -20,4 +20,5 @@ pcie("${SLOT}")
 
 iocInit
 
-< "iocBoot/${IOC}/autosave_post.cmd"
+create_monitor_set("fofb_ioc.req", 30, "P=${P}, R=${R}, S=${S}")
+set_savefile_name("fofb_ioc.req", "${P}${R}_settings.sav")
