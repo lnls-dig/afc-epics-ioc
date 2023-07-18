861b3388fcf54aed0b7d98b903fc32322c995b62
Érico Nogueira <erico.rolim@lnls.br>
2023-07-18 17:03:42 -0300

    iocutca: add stTIM.cmd and runTIM.sh.
    tim-slot-mapping was copied from [1].
    
    We add EPICS_CA_ADDR_LIST pointing to our PV gateway because we are
    using a PV from the accelerator in our development crates.
    
    [1] https://github.com/lnls-dig/tim-rx-epics-ioc/tree/devel

---
+++ iocBoot/iocutca/stTIM.cmd
@@ -0,0 +1,17 @@
+#!../../bin/linux-x86_64/utca
+
+< common.cmd
+
+pcie("${SLOT}")
+
+< "iocBoot/$(IOC)/build_info.cmd"
+
+AfcTiming(0)
+dbLoadTemplate("db/afc_timing.substitutions", "P=${P}, R=${R}, PORT=AFC_TIMING-0")
+
+< "iocBoot/${IOC}/autosave_pre.cmd"
+
+iocInit
+
+create_monitor_set("tim_ioc.req", 30, "P=${P}, R=${R}")
+set_savefile_name("tim_ioc.req", "${P}${R}_settings.sav")
