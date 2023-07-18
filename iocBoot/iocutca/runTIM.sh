647b525f9b988d6be3803b0c0aad8fe98e83d1d3
Érico Nogueira <erico.rolim@lnls.br>
2023-07-18 17:03:42 -0300

    iocutca: add stTIM.cmd and runTIM.sh.
    tim-slot-mapping was copied from [1].
    
    We add EPICS_CA_ADDR_LIST pointing to our PV gateway because we are
    using a PV from the accelerator in our development crates.
    
    [1] https://github.com/lnls-dig/tim-rx-epics-ioc/tree/devel

---
+++ iocBoot/iocutca/runTIM.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -eu
+
+export SLOT=$1
+
+CRATE=$(./getCrate.sh)
+VSLOT=$(./getSlot.sh $SLOT)
+
+CRATE=${CRATE#0}
+
+. ./tim-slot-mapping
+val_name=CRATE_${CRATE}_TIM_RX_${VSLOT}_PV_AREA_PREFIX
+eval "export AREA_PREFIX=\$${val_name}"
+val_name=CRATE_${CRATE}_TIM_RX_${VSLOT}_PV_DEVICE_PREFIX
+eval "export DEVICE_PREFIX=\$${val_name}"
+
+export EPICS_CA_ADDR_LIST=10.0.38.59:60000
+
+exec procServ -f -n tim${SLOT} -L- -i ^C^D -P $(expr 1700 + ${VSLOT}) ./stTIM.cmd
