7b4c70d70d2739987c6a8ba902cba6b28021cb43
Érico Nogueira <erico.rolim@lnls.br>
2023-07-25 14:08:29 -0300

    iocutca: support accelerator crates in runTIM.sh.
    Crates in our accelerator network don't need to (and shouldn't) talk to
    the CA Gateway in order to obtain the FPGAClk-Cte PV.

--- iocBoot/iocutca/runTIM.sh
+++ iocBoot/iocutca/runTIM.sh
@@ -15,6 +15,9 @@ eval "export AREA_PREFIX=\$${val_name}"
 val_name=CRATE_${CRATE}_TIM_RX_${VSLOT}_PV_DEVICE_PREFIX
 eval "export DEVICE_PREFIX=\$${val_name}"
 
-export EPICS_CA_ADDR_LIST=10.0.38.59:60000
+case $(hostname) in
+  ia-*) ;;
+  *) export EPICS_CA_ADDR_LIST=10.0.38.59:60000
+esac
 
 exec procServ -f -n tim${SLOT} -L- -i ^C^D -P $(expr 1700 + ${VSLOT}) ./stTIM.cmd
