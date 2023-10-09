21f73cf9c6bc53405a888d3854368074cf4a91fc
Érico Nogueira <erico.rolim@lnls.br>
2023-10-09 12:24:29 -0300

    iocutca: remove DCC acq modules.

--- iocBoot/iocutca/sysid.cmd
+++ iocBoot/iocutca/sysid.cmd
@@ -1,2 +1,2 @@
 SysId(0)
-dbLoadTemplate("db/sysid.substitutions", "P=${P}, R=${R}, S=${S}, NELM=${NELM}, PORT=SYSID-0, ACQ_NAME=SYSID, ACQ_PORT=ACQ-3")
+dbLoadTemplate("db/sysid.substitutions", "P=${P}, R=${R}, S=${S}, NELM=${NELM}, PORT=SYSID-0, ACQ_NAME=SYSID, ACQ_PORT=ACQ-1")
