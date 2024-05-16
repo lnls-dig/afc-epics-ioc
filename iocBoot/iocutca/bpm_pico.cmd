7c996570a4a05a451808f58b57edc7f0e6c1c1ff
Érico Nogueira <erico.rolim@lnls.br>
2024-05-16 12:23:16 -0300

    iocutca: add PBPM startup script.

---
+++ iocBoot/iocutca/bpm_pico.cmd
@@ -0,0 +1,5 @@
+FMCPico(0)
+dbLoadTemplate("db/fmcpico1m_4ch.substitutions", "P=${P1}, R=${R1}, PORT=FMC_PICO-0")
+
+FMCPico(1)
+dbLoadTemplate("db/fmcpico1m_4ch.substitutions", "P=${P2}, R=${R2}, PORT=FMC_PICO-1")
---
+++ iocBoot/iocutca/stPBPM.cmd
@@ -0,0 +1,34 @@
+#!../../bin/linux-x86_64/utca
+
+< common.cmd
+
+pcie("${SLOT}")
+
+epicsEnvSet("P1", "${AREA_PREFIX_1}")
+epicsEnvSet("P2", "${AREA_PREFIX_2}")
+
+epicsEnvSet("R1", "${DEVICE_PREFIX_1}")
+epicsEnvSet("R2", "${DEVICE_PREFIX_2}")
+
+epicsEnvSet("P", "${P1}")
+epicsEnvSet("R", "${R1}")
+
+asSetSubstitutions("P=$(P),R=$(R)")
+
+< "iocBoot/$(IOC)/build_info.cmd"
+< "iocBoot/${IOC}/orbit_intlk.cmd"
+< "iocBoot/${IOC}/triggers.cmd"
+
+var reToolsVerbose 0
+reAddAlias "${P1}${R1}(GwInfo.*)" "${P2}${R2}$1"
+reAddAlias "${P1}${R1}(Intlk.*)" "${P2}${R2}$1"
+reAddAlias "${P1}${R1}(TRIGGER[[:digit:]].*)" "${P2}${R2}$1"
+
+< "iocBoot/${IOC}/bpm_acq.cmd"
+< "iocBoot/${IOC}/bpm_swap.cmd"
+< "iocBoot/${IOC}/bpm_pico.cmd"
+< "iocBoot/${IOC}/bpm_pos.cmd"
+
+iocInit
+
+< "iocBoot/${IOC}/apply_asg.cmd"
